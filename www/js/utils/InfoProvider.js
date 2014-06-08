2297f86fb988a3820c847cfb9ce1850795338550
GiorgioNatili <g.natili@gnstudio.com>
2014-06-08 07:06:47 +0200

    Handling of the not existing logo

--- www/js/utils/InfoProvider.js
+++ www/js/utils/InfoProvider.js
@@ -1,6 +1,6 @@
 define('utils/InfoProvider', ['appframework', 'appframeworkui', 'core/Constants', 'model/DataProvider', 'i18n!nls/miscellaneous'], (function($, $ui, Constants, dataProvider, miscellaneous){
 
-    var events = {API_INFO_DATA_READY: 'onInfoDataReady'};
+    var events = {API_INFO_DATA_READY: 'infoDataReady', API_INFO_DATA_ERROR: 'infoDataError'};
 
     var logoURL, dataCache;
     var that = this;
@@ -22,10 +22,10 @@ define('utils/InfoProvider', ['appframework', 'appframeworkui', 'core/Constants'
     
       navigator.notification.confirm(miscellaneous.genericMissingInfoError,
                                       function(buttonIndex){
-                                     
+
                                         if(buttonIndex === 1){
                                      
-                                            dataReady();
+                                            dataReady(true);
                                      
                                         }else{
                                      
@@ -36,25 +36,13 @@ define('utils/InfoProvider', ['appframework', 'appframeworkui', 'core/Constants'
                                      
                                       }, miscellaneous.confirmTitle,
                                          [miscellaneous.ok, miscellaneous.cancel]);
-                                     
-      
-      
-     /* confirmCallback, [title], [buttonLabels])
-
-        navigator.notification.alert(miscellaneous.genericError, function(){
-
-            $ui.hideMask();
-            $ui.loadContent('main', false, false, Constants.PANELS_DIRECTION);
-
-        });*/
-
     };
 
     var getInfo = function(url, rootURL){
 
         if(dataCache && logoURL){
 
-            dataReady();
+            dataReady(false);
 
         }else{
 
@@ -67,11 +55,22 @@ define('utils/InfoProvider', ['appframework', 'appframeworkui', 'core/Constants'
 
     };
 
-    var dataReady = function(){
+    var dataReady = function(error){
 
-        // console.log('ON DATA READY', logoURL, this, this.dispatchEvent)
+        var eventType;
 
-        var event = new CustomEvent(events.API_INFO_DATA_READY, {bubbles: true, cancelable: true});
+        if(error){
+
+            eventType = events.API_INFO_DATA_ERROR;
+
+        }else{
+
+            eventType = events.API_INFO_DATA_READY;
+
+        }
+
+
+        var event = new CustomEvent(eventType, {bubbles: true, cancelable: true});
         that.dispatchEvent(event);
 
     };
